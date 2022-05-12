8afaa0c29118a468402d6567fdb86bd1a469ed26
thientv <thientv.cs@gmail.com>
2022-05-12 07:56:29 +0700

    up

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,6 @@
 const {configGenerator} = require("./webpack.generator.js");
 
 module.exports = (env, options) => [
-	configGenerator(
-		options, 
-		'glb-generator',
-		[
-			'index', 
-		], 
-		[
-		]
-	)
+	configGenerator(options, 'glb-generator', ['index'], []),
+	configGenerator(options, 'load-asset-3d', ['index'], [])
 ];
