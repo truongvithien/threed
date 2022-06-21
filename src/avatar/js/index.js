dbd936492aafe96c82ad8b4e49c2940ced8fea5f
Trương Vĩ Thiên <thientv.cs@gmail.com>
2022-06-21 15:40:04 +0700

    up

--- src/avatar/js/index.js
+++ src/avatar/js/index.js
@@ -4,19 +4,26 @@ import {avatar, debug} from "./avatar/main";
 
 $(function () {
     avatar.init();
-    avatar.switch();
-
-    debug.freeCamera();
+    // avatar.switch(); // --> Active switch 2D / 3D
+    // debug.freeCamera(); // --> Active free camera mode (for debugging)
 });
 
 window.addEventListener("load", function(){
-    // avatar.loadSMT("SMT1");
+
+    // === ACTIVE DIRECTIVELY
+
+    // var st_code = "HM3-EM3-FM3-OM2-A1-T3";
+    // avatar.loadST(st_code);
+
+
+
+
+    // === ACTIVE BY URL PARAMS
 
     var url = new URL(window.location.href);
     var smt_code = url.searchParams.get("load_smt");
     if (typeof smt_code != "undefined" && smt_code != null) {
         if (smt_code.length == 0) smt_code = "SMT1";
-        // avatar.loadDefaultBg();
         avatar.loadSMT(smt_code);
     }
 
@@ -25,8 +32,6 @@ window.addEventListener("load", function(){
     if (typeof st_code != "undefined" && st_code != null) {
         // if (st_code.length == 0) st_code = "HM3-EM3-FM3-OM2-A1-T3";
         if (st_code.length == 0) st_code = "HM3-EM3-FM3-OM2-A1-T2";
-        // avatar.loadDefaultBg();
-        // avatar.loadST(st_code); 
 
         var anim_code = url.searchParams.get("anim");
         if (typeof anim_code != "undefined" && anim_code != null) {
@@ -42,21 +47,13 @@ window.addEventListener("load", function(){
     if (typeof stdemo_code != "undefined" && stdemo_code != null) {
         // if (st_code.length == 0) st_code = "HM3-EM3-FM3-OM2-A1-T3";
         if (stdemo_code.length == 0) stdemo_code = "HM3-EM3-FM3-OM2-A1-T1";
-        // avatar.loadDefaultBg();
         avatar.loadSTDEMO(stdemo_code); 
     }
 
     var dim = url.searchParams.get("dim");
     // console.log(dim);
     switch (dim) {
-        case "2": 
-            if ($("#layered_avatar").length > 0) {
-                $("#layered_avatar").addClass("active");
-                $("#switch_avatar").prop("checked", true);
-                $("#switch_avatar + label .dim").html(3);
-            }
-            break;
-        case "3": default: 
+        case "3":
             if ($("#rendered_avatar").length > 0) {
                 $("#rendered_avatar").addClass("active");
                 $("#switch_avatar").prop("checked", false);
@@ -64,15 +61,16 @@ window.addEventListener("load", function(){
             }
             
             break;
+        case "2": default:
+            if ($("#layered_avatar").length > 0) {
+                $("#layered_avatar").addClass("active");
+                $("#switch_avatar").prop("checked", true); 
+                $("#switch_avatar + label .dim").html(3);
+            }
+            break;
     }
 
-    
-    // var color_correction = url.searchParams.get("load_smt");
-    // if (typeof color_correction != "undefined" && color_correction != null) {
-        
-    // }
 
 });
 
- 
 window.debug = debug;
--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,8 @@ const {configGenerator} = require("./webpack.generator.js");
 
 module.exports = (env, options) => [
 	configGenerator(options, 'doc', ['index'], []),
-	configGenerator(options, 'glb-generator', ['index'], []),
-	configGenerator(options, 'load-asset-3d', ['index'], []),
-	configGenerator(options, 'obj-smt', ['index'], []),
+	// configGenerator(options, 'glb-generator', ['index'], []),
+	// configGenerator(options, 'load-asset-3d', ['index'], []),
+	// configGenerator(options, 'obj-smt', ['index'], []),
 	configGenerator(options, 'avatar', ['index'], [])
-];
+]; 
